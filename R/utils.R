2c0a6ac40eb924d425a3dac5b4be207fc67d29fd
SALOMON JEMAY <jemay.salomon@inrae.fr>
2025-03-22 20:01:42 +0100

    minor edits

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,13 @@
-#' Get the Version of the tmbExtract Package
-#'
-#' This function returns the version of the `tmbExtract` package.
-#'
-#' @return A list containing the package version.
-#' @export
-#' @author Jemay Salomon
-#' 
-#' @examples
-#' tmbExtract.version()
+##' Get the Version of the tmbExtract Package
+##'
+##' This function returns the version of the `tmbExtract` package.
+##'
+##' @return A list containing the package version.
+##' @author Jemay Salomon
+##' 
+##' @examples
+##' tmbExtract.version()
+##' @export
 tmbExtract.version <- function() {
   list(package = packageVersion("tmbExtract"))
 }
