70e9e8dbf6eee6c1cae81a5ef74c6f218c727514
SALOMON JEMAY <jemay.salomon@inrae.fr>
2025-10-20 15:05:23 +0200

    update file & workflows

--- tests/testthat/test_example.R
+++ tests/testthat/test_example.R
@@ -4,7 +4,7 @@ library(TMB)
 test_that("ExtractParamsTmb", {
   
   path = file.path(system.file("examples",package="TMB"))
-  TMB::compile(file.path(path, "simple.cpp"), clean = TRUE)
+  TMB::compile(file.path(path, "simple.cpp"))
   out <- runExample("simple")
   
   out_test <- list()
