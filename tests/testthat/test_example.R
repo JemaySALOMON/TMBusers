226e52a64be9d2f43926a9dc2e479c2423680c4c
SALOMON JEMAY <jemay.salomon@inrae.fr>
2025-10-20 16:07:31 +0200

    minor edits

--- tests/testthat/test_example.R
+++ tests/testthat/test_example.R
@@ -22,10 +22,8 @@ test_that("ExtractParamsTmb", {
     "logsdu" = -0.15777145,
     "logsd0" = 0.03326068
   )
-  
   expect_equal(observed, expected)
 })
-
 
 
 test_that("ExtractCorTmb", {
@@ -38,27 +36,23 @@ test_that("ExtractCorTmb", {
 test_that("ExtractRandTmb", {
   expected <- c(0)
   observed <- c(0)
-  
   expect_equal(observed, expected)
 })
 
 test_that("ExtractStdTmb", {
   expected <- c(0)
   observed <- c(0)
-  
   expect_equal(observed, expected)
 })
 
 test_that("ExtractVarTmb", {
   expected <- c(0)
   observed <- c(0)
-  
   expect_equal(observed, expected)
 })
 
 test_that("tmbExtract", {
   expected <- c(0)
   observed <- c(0)
-  
   expect_equal(observed, expected)
 })
